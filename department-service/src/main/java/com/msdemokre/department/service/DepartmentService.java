f92c7bc51bd63b9eca9a783a67513be96befbf3e
Rajendran Kreshan <kreshan882@gmail.com>
2022-10-23 13:51:57 +0530

    commit fst

---
+++ department-service/src/main/java/com/msdemokre/department/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.msdemokre.department.service;
+
+import com.msdemokre.department.entity.Department;
+import com.msdemokre.department.repositery.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
